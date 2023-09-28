8532a6d12ada35d7dab94bc1e984b448368a19d7
kravetstwo <146323720+kravetstwo@users.noreply.github.com>
2023-09-28 12:13:12 +0300

    fix: some errors in service_configuration are fixed

--- src/service_configuration.py
+++ src/service_configuration.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 
-import string
 import time
 import os
 import random
+from tqdm import tqdm
 
 configuration_number = "1xM18650"
 version = "1.0.0"
@@ -30,7 +30,7 @@ time.sleep(0.5)
 print("Service package 1: start configuration ")
 time.sleep(0.5)
 
-for i in tqdm.tqdm(range(3000), ascii=True, desc="System check"):
+for i in tqdm(range(3000), ascii=True, desc="System check"):
     time.sleep(0.001)
 
 print("")
